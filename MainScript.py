d2456050f340f79315a0ab7c374e3f1bb4c094e9
Mahmoud Suliman <86120671+MahmoudSuliman@users.noreply.github.com>
2021-09-09 21:03:08 +0200

    Update MainScript.py

--- MainScript.py
+++ MainScript.py
@@ -2,23 +2,26 @@
 """
 Created on Thu Sep  2 15:24:33 2021
 
-@author: KIDDO
+@author: MahmoudSuliman
 """
 
 # =============================================================================
 # 
 
 import os
-os.chdir(r'C:\Users\KIDDO\Downloads\SU Study\Traineeship\Urban Heat Island\Data_22T_23P\1.Tullinge A\Historiska_ortofoton_1960_PAN_tif__d9c09fff-9a63-40a6-96fe-0f5046249428_')
+os.chdir(r'C:\Users\KIDDO\Downloads\SU Study\Traineeship\Urban Heat Island\Data_22T_23P\2.Södertälje\Historiska_ortofoton_1960_PAN_tif__493271c1-5839-4fc1-b9cb-081f4f83da6d_')
 import matplotlib.image as img
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 import pandas as pd
+import glob
 import PIL
 
 import rasterio
 from rasterio.plot import show
 from rasterio.plot import show_hist
+from rasterio.merge import merge
 from rasterio.mask import mask
 from shapely.geometry import box
 import geopandas as gpd
@@ -26,30 +29,63 @@ from fiona.crs import from_epsg
 from osgeo import gdal
 import pycrs
 
-# import georaster uninstalled
+# import georaster
 # conda install -c conda-forge georaster
+
+swer='+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 # =============================================================================
-# PIL.Image.MAX_IMAGE_PIXELS = 100000000
+# Raster Mosaic and preparation
 
-# # RasterIO Method
-# fp = r'656_66_05.tif'
-# image = rasterio.open(fp)
-# show(image)
+############# add an if statement to this so that it does not automatically
+############# mosaics
 
-# # GDAL & Matplotlib Method
-# dataset = gdal.Open(fp, gdal.GA_ReadOnly) 
+# File and folder paths
+dirpath = r'C:\Users\KIDDO\Downloads\SU Study\Traineeship\Urban Heat Island\Data_22T_23P\2.Södertälje\Historiska_ortofoton_1960_PAN_tif__493271c1-5839-4fc1-b9cb-081f4f83da6d_'
+out_fp = r'Mosaic.tif'
 
-# # Note GetRasterBand() takes band no. starting from 1 not 0
-# band = dataset.GetRasterBand(1)
-# arr = band.ReadAsArray()
-# plt.imshow(arr)
+# Make a search criteria to select the orthophotos
+search_criteria = "656*.tif"
+q = os.path.join(dirpath, search_criteria)
 
+# print(q)
+
+# Searching for our files using the glob function
+dem_fps = glob.glob(q)
+# dem_fps
+
+# creating an empty list for mosaic components
+src_files_to_mosaic = []
+
+# opening the mosaic components (tif) in read mode with RasterIO
+for fp in dem_fps:
+    src = rasterio.open(fp)
+    src_files_to_mosaic.append(src)
+
+# src_files_to_mosaic
+
+# Merging 
+mosaic, out_trans = merge(src_files_to_mosaic)
+
+# displaying the results
+show(mosaic, cmap='gray')
+
+# Copy the metadata
+out_meta = src.meta.copy()
+
+# Update the metadata
+out_meta.update({"driver": "GTiff", "height": mosaic.shape[1],
+                 "width": mosaic.shape[2], "transform": out_trans,
+                 "crs": swer})
+
+# Write the mosaic RasterIO to disk
+with rasterio.open(out_fp, "w", **out_meta) as dest:
+    dest.write(mosaic)
 # =============================================================================
 # Clipping
 
 # Filepaths
-fp = r'656_66_05.tif'
-out_tif = r'ClipExp.tif'
+fp = r'Mosaic.tif'
+out_tif = r'Clip.tif'
 
 # opening the raster
 data = rasterio.open(fp)
@@ -57,9 +93,9 @@ data = rasterio.open(fp)
 # plotting the data
 show(data, cmap='gray')
 
-# Tullinge A Coordinates:
-# Decimal (lat,lon): (59.1785, 17.9093) 
-# Northing easting (E,N): (666246.163183566, 6563554.18604587) 
+# Södertälje Coordinates:
+# Decimal (lat,lon): (59.2141, 17.6291) 
+# Northing easting (E,N): (650084.04312309, 6566851.5500514) 
 
 # creating a boundary box with shapely around the Area of interest
 # WGS84 coordinates
@@ -68,9 +104,9 @@ show(data, cmap='gray')
 # minx, miny = 17.9093-x, 59.1785-x
 # maxx, maxy = 17.9092+x, 59.1785+x
 
-x=1000
-minx, miny = 666246.163183566-x, 6563554.18604587-x
-maxx, maxy = 666246.163183566+x, 6563554.18604587+x
+x=300 #600*600m
+minx, miny = 650084.04312309-x, 6566851.5500514-x
+maxx, maxy = 650084.04312309+x, 6566851.5500514+x
 
 bbox = box(minx, miny, maxx, maxy)
 
@@ -100,8 +136,8 @@ print(out_meta)
 
 # Parsing EPSG code from the (coordinate reference system) CRS (creating a Proj4 string to ensure 
 # that projection information is saved properly)
-epsg_code = int(data.crs.data['init'][5:])
-print(epsg_code)
+# epsg_code = int(data.crs.data['init'][5:])
+# print(epsg_code)
 
 # updating the metadata with new dimensions, transform (affine) 
 # and CRS (as Proj4 text). NOTE: the crs code here manually reprojects it with
@@ -121,4 +157,47 @@ clipped = rasterio.open(out_tif)
 show(clipped, cmap='gray')
 
 # =============================================================================
+# =============================================================================
+# Classification
 
+driver = gdal.GetDriverByName('GTiff')
+file = gdal.Open(r'Clip.tif')
+band = file.GetRasterBand(1)
+lista = band.ReadAsArray()
+
+# reclassification
+lista[np.where( lista <= 0 )] = 1
+lista[np.where((0 < lista) & (lista <= 70)) ] = 2
+lista[np.where((70 < lista) & (lista <= 105)) ] = 3
+lista[np.where((105 < lista) & (lista <= 200)) ] = 4
+lista[np.where( lista > 200 )] = 5
+
+# create new file
+file2 = driver.Create( 'Classified2.tif', file.RasterXSize , file.RasterYSize , 1)
+file2.GetRasterBand(1).WriteArray(lista)
+
+# spatial ref system
+proj = file.GetProjection()
+georef = file.GetGeoTransform()
+file2.SetProjection(proj)
+file2.SetGeoTransform(georef)
+file2.FlushCache()
+
+# Filepaths
+fp = r'Classified2.tif'
+
+# opening the raster
+data = rasterio.open(fp)
+
+# creating a colormap
+
+colors = ['darkgreen', 'forestgreen', 'honeydew', 'lightgray']  # R -> G -> B
+n_bins = [3, 6, 10, 100]  # Discretizes the interpolation into bins
+cmap_name = 'my_list'
+for n_bin, ax in zip(n_bins, axs.ravel()):
+    cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
+
+# plotting the data
+show(data, cmap=cm)
+
+# =============================================================================
