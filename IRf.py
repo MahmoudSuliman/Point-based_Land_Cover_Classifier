d343a6b4ea9513f2acff58b50f38b5d3e4166984
Mahmoud Suliman <86120671+MahmoudSuliman@users.noreply.github.com>
2021-12-25 07:12:26 +0100

    Update IRf.py

--- IRf.py
+++ IRf.py
@@ -201,78 +201,178 @@ show(clipped, cmap='gray')
 # =============================================================================
 # Classification
 
-landsat_path = glob("Mosaic.tif")
-
-landsat_path.sort()
-
-for idx, f in enumerate(landsat_path):
-    print(f"{idx}: {f}")
-
 # GDAL file and band definitions
 driver = gdal.GetDriverByName('GTiff')
 file = gdal.Open(r'Clip.tif')
 
+# loading the different bands
+
+# IR
 bandir = file.GetRasterBand(2)
 b1 = listair = bandir.ReadAsArray()
 
+# red
 bandred = file.GetRasterBand(3)
 b2 = listared = bandred.ReadAsArray()
 
+# green
 bandgreen = file.GetRasterBand(1)
 b3 = listagreen = bandgreen.ReadAsArray()
 
+# plotting the bands
 plt.imshow(listair)
 plt.imshow(listared)
 plt.imshow(listagreen)
 
-ndvi_l8 = es.normalized_diff(b1, b2)
-ep.plot_bands(ndvi_l8, cmap="RdYlGn", cols=1, vmin=-1, vmax=1)
+# calculating ndvi
+ndvi = es.normalized_diff(b1, b2)
 
-# =============================================================================
-# =============================================================================
-# 
-# ndvi_class_bins = [-np.inf, 0, 0.33, 0.36, np.inf]
+# test plot
+ep.plot_bands(ndvi, cmap="RdYlGn", cols=1, vmin=-1, vmax=1)
 
-ndvi_class_bins = [-np.inf, 0.14, 0.27, 0.36, np.inf]
-
-ndvi_landsat_class = np.digitize(ndvi_l8, ndvi_class_bins)
+# binning
+ndvi_class_bins = [-np.inf, 0.14, 0.27, 0.36, np.inf] # defining bins
+ndvi_landsat_class = np.digitize(ndvi, ndvi_class_bins) # binning the array
 
 # Apply the nodata mask to the newly classified NDVI data
-ndvi_landsat_class = np.ma.masked_where(np.ma.getmask(ndvi_l8), ndvi_landsat_class)
+ndvi_landsat_class = np.ma.masked_where(np.ma.getmask(ndvi), ndvi_landsat_class)
 
 # and check the number of unique values in our now classified dataset
 np.unique(ndvi_landsat_class)
 
+# ep plotting -----------------------------------------------------------------
+ 
 # Define color map
 nbr_colors = ["lightgray", "honeydew", "forestgreen", "darkgreen"]
-
-# nbr_colors = ['lightblue',"lightgray", 'tan',"honeydew", "forestgreen", "darkgreen"]
-
 nbr_cmap = ListedColormap(nbr_colors)
 
 # Define class names
 ndvi_cat_names = ["Urban Areas","light to no vegetation","intermediate Vegetation","Heavy Vegetation"]
 
-
 # Get list of classes
-classes_l8 = np.unique(ndvi_landsat_class)
-
-classes_l8 = classes_l8.tolist()
+classes = np.unique(ndvi_landsat_class)
+classes = classes.tolist()
 
 # The mask returns a value of none in the classes. remove that
-classes_l8 = classes_l8[0:4]
+classes = classes[0:4]
 
-# Plot your data
+# Plotting
 fig, ax = plt.subplots(figsize=(12, 12))
-
 im = ax.imshow(ndvi_landsat_class, cmap=nbr_cmap)
-
-ep.draw_legend(im_ax=im, classes=classes_l8, titles=ndvi_cat_names)
-
+ep.draw_legend(im_ax=im, classes=classes, titles=ndvi_cat_names)
 ax.set_title("Landsat 8 - Normalized Difference Vegetation Index (NDVI) Classes", fontsize=14)
-# Text(0.5, 1.0, 'Landsat 8 - Normalized Difference Vegetation Index (NDVI) Classes')
-
 ax.set_axis_off()
-
 plt.tight_layout()
+
+# old classification
+# band calculation based on ndvi eq
+lista=(b1-b2)/(b1+b2)
+
+ndvi_class_bins = [-np.inf, 0.14, 0.27, 0.36, np.inf] # defining bins
+lista = np.digitize(ndvi, ndvi_class_bins) # binning the array
+
+plt.imshow(lista)
+
+lista[np.where( lista <= 0.14 )] = 4 # Nan
+lista[np.where((0.14 < lista) & (lista <= 0.27)) ] = 3 # I.veg
+lista[np.where((0.27 < lista) & (lista <= 0.36)) ] = 2 # L.veg
+lista[np.where( lista > 0.36 )] = 1 # urban'''
+
+plt.imshow(ndvi_landsat_class)
+
+# creating new file
+file2 = driver.Create( 'Classified4.tif', file.RasterXSize , file.RasterYSize , 1)
+file2.GetRasterBand(1).WriteArray(lista)
+
+# Spatial referencing system obs
+proj = file.GetProjection()
+georef = file.GetGeoTransform()
+file2.SetProjection(proj)
+file2.SetGeoTransform(georef)
+file2.FlushCache()
+
+# Adding  and opening output's filepath
+fp = r'Classified4.tif'
+data = rasterio.open(fp)
+
+# creating a custom colormap
+colors = ['lightgray','honeydew','forestgreen','darkgreen']  # R -> G -> B
+n_bins = [1, 2, 3, 4]  # Discretizes the interpolation into bins
+cmap_name = 'my_list'
+fig, axs = plt.subplots(2, 2, figsize=(6, 9))
+for n_bin, ax in zip(n_bins, axs.ravel()):
+    cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bin)
+
+# plotting the data
+fig, ax = plt.subplots(figsize=(10,8))
+image_hidden = ax.imshow(data.read()[0], cmap=cm)
+cbar= fig.colorbar(image_hidden, ax=ax, ticks=[1.475,2.45,3.6,4.55])
+cbar.ax.yaxis.set_tick_params(width=0)
+plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
+plt.gcf().axes[0].xaxis.get_major_formatter().set_scientific(False)
+cbar.ax.set_yticklabels(['Urban Areas','light to no Vegetation','Intermediate Vegetation\nand shadows',
+                         'Heavy vegetation \nand water bodies'], fontsize=16, weight='bold')  # vertically oriented colorbar
+show(data, ax=ax, cmap=cm)
+plt.savefig('ClassifiedColored.jpg', dpi=300, bbox_inches='tight')
+
+# cropping the color bar
+cbim = Image.open('ClassifiedColored.jpg')
+cbim_crop = cbim.crop((2100, 0, 3145, 1947))
+
+# Plotting and saving the cropped colorbar
+fig, ax = plt.subplots(figsize=(10,10.5))
+plt.imshow(cbim_crop)
+plt.axis('off') # removes ticks and border (spines)
+plt.savefig('CBar.jpg', dpi=300, bbox_inches='tight')
+# =============================================================================
+# =============================================================================
+# Circular cutting 
+# classified
+fp = r'Classified4.tif'
+data = rasterio.open(fp)
+
+# removing ticks and converting the image for pillow
+fig, ax = plt.subplots(figsize=(10,10))
+image_hidden = ax.imshow(data.read()[0], cmap=cm)
+show(data, ax=ax, cmap=cm)
+plt.xticks([])
+plt.yticks([])
+plt.savefig('Pillimge.jpg', dpi=300, bbox_inches='tight')
+
+# Cropping using pillow
+cimg=Image.open('Pillimge.jpg')
+height,width = cimg.size
+lum_img = Image.new('L', [height,width] , 0)  
+draw = ImageDraw.Draw(lum_img)
+draw.pieslice([(50,30), (height-30,width-50)], 0, 360, 
+              fill = 255, outline = "white")
+img_arr =np.array(cimg)
+lum_img_arr =np.array(lum_img)
+final_img_arr = np.dstack((img_arr,lum_img_arr))
+display(Image.fromarray(final_img_arr))
+Image.fromarray(final_img_arr).save('CircleUrkle.png')
+
+# convert image to remove background data (no data)
+image = Image.open('CircleUrkle.png').convert('RGBA')
+new_image = Image.new("RGBA", image.size, "WHITE") # Create a white rgba background
+new_image.paste(image, (0, 0), image)              # Paste the image on the background. Go to the links given below for details.
+new_image.convert('RGB').save('CircleUrkle.jpg')  # Save as JPEG
+
+# plotting full cbar image
+
+im1 = Image.open('CircleUrkle.jpg')
+im2 = Image.open('CBar.jpg')
+
+def get_concat_h(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+get_concat_h(im1, im2).save('WorkingCBar.jpg')
+
+plt.imshow(Image.open('WorkingCBar.jpg'))
+plt.axis('off') # removes ticks and border (spines)
+plt.savefig('fullCircleCBar.jpg', dpi=300, bbox_inches='tight')
+
 # =============================================================================
